88f2aa8d07d66247ee591c013d54730924c1733a
KirillReal <kiril5471710@gmail.com>
2023-08-11 19:29:33 +0300

    added removeDublicates for int array task

---
+++ src/com/education/Test.java
@@ -0,0 +1,22 @@
+package com.education;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {0, 5, 10, 10, 16, 22, 22, 23};
+        removeDuplicates(arr);
+    }
+
+    public static void removeDuplicates(int a[])
+    {
+        int j = 0;
+        for (int i = 1; i < a.length; i++) {
+            if (a[i - 1] != a[i]) {
+                a[j++] = a[i];
+                System.out.println(a[i]);
+            }
+        }
+
+        a[j++] = a[a.length - 1];
+    }
+
+}
